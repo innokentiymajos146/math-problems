b241969a22538f4afcc9c5b23c06f9583d820a05
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-03-24 19:54:49 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+        
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
