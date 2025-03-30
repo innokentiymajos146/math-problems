3fde5f87d4e4d10f7ad306d468e11881ab929862
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-03-30 20:50:22 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,17 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Sum of two numbers
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum is: " + (a + b));
+
+        // Example 2: Multiply two numbers
+        double result = 4.5 * 2;
+        System.out.println("Result: " + result);
+
+        // Example 3: Square root of a number
+        int num = 9;
+        double sqrt = Math.sqrt(num);
+        System.out.println("Square root is: " + sqrt);
+    }
+}
