bf216e86383da8769c86fd6c6948562af1a15d41
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-03 13:08:43 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,25 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Simple addition
+        System.out.println("Result of adding 5 and 3 is: " + (5 + 3));
+
+        // Example 2: Simple subtraction
+        System.out.println("Result of subtracting 7 from 9 is: " + (9 - 7));
+
+        // Example 3: Simple multiplication
+        System.out.println("Result of multiplying 4 and 6 is: " + (4 * 6));
+
+        // Example 4: Simple division
+        System.out.println("Result of dividing 15 by 3 is: " + (15 / 3));
+
+        // Example 5: Using an input statement for a mathematical expression
+        double a = 2.0;
+        double b = 3.0;
+        System.out.println("Result of calculating the square root of " + a + " squared by " + b + " is: " + Math.sqrt(a * b));
+
+        // Example 6: Using an input statement for a mathematical expression
+        String x = "4";
+        double y = 1.0;
+        System.out.println("Result of calculating the factorial of " + x + "! by " + y + " is: " + Math.factorial(x));
+    }
+}
