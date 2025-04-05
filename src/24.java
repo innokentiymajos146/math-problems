8f31dc7e0ace562aff1e276bdbc1c92064bde6dc
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-05 05:33:32 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,12 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        double product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
