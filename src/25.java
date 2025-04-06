803831e93d6d6042cc0c65b4e63ce08aed828635
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-06 06:16:14 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,12 @@
+public class MathProblems {
+    // Method to calculate the area of a rectangle
+    public static double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    // Method to add two numbers and round the result to 2 decimal places
+    public static double addAndRoundNumbers(double num1, double num2) {
+        double roundedResult = Math.round(num1 + num2);
+        return roundedResult;
+    }
+}
