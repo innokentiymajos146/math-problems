8e82ade54c38b295f5f55b0d0d1f5ec7354f117d
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-08 05:14:24 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example problem: Calculate the sum of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+    }
+}
