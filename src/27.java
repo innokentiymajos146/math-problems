665841c7b254568fcf5d5e21c24f82172ea92dff
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-09 07:56:39 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of solving a simple linear equation: 3x + 5 = 14
+        double x = (14 - 5) / 3;
+        System.out.println("The solution is: " + x);
+    }
+}
