3644a8a7b0d245a04c27763e6eecef1378804b53
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-23 10:56:55 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code to solve a simple math problem
+        double a = 5.0;
+        double b = 2.0;
+        double result = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + result);
+    }
+}
