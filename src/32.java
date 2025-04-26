25d87c14c4784296e78e91f81ee7ac275b67a312
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-26 03:51:12 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        float z = 10.0f;
+        
+        System.out.println("The value of x + y: " + (x + y));
+        System.out.println("The value of z squared: " + Math.pow(z, 2));
+    }
+}
