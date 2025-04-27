89ce84b771bb94014d41f5b1922613c44e23911e
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-04-27 05:30:26 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.5;
+        System.out.println("x + y: " + (x + y));
+        System.out.println("x * y: " + (x * y));
+        System.out.println("Math.sqrt(x): " + Math.sqrt(x));
+    }
+}
