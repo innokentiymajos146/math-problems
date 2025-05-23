3795abe6d6d3069c479398fefedc3170a2c650f6
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-05-23 05:04:56 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of solving basic arithmetic problems
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+        
+        // Example of solving simple geometry problems
+        double area = 10 * 7 / 2;
+        System.out.println("The area is: " + area);
+    }
+}
