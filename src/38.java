a6f98bc74f0779e7c759e619256612dc0006295b
innokentiymajos146 <innokentiymajos146@gmail.com>
2025-05-24 05:09:43 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 3.14;
+        float c = 20f;
+        
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+        System.out.println("The value of c is " + c);
+    }
+}
